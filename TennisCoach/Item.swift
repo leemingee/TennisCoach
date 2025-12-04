c259ba7d16aa4cf3c7038e5a6ac173a7109b7b56
leemingee <leemingee1995@gmail.com>
2025-12-04 12:28:10 -0800

    Initial Commit

---
+++ TennisCoach/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TennisCoach
+//
+//  Created by Yoyo on 12/4/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
